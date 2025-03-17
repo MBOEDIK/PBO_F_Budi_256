8a19b65d706ed63cb5adc9b0ac7ebefd4fd7ef1f
MBOEDIK65 <boediksm65@gmail.com>
2025-03-18 02:39:14 +0700

    ngerjain tugas 2

--- Tugas/Tugas 1/src/Main.java
+++ Tugas/Tugas 1/src/Main.java
@@ -1,147 +1,84 @@
 import java.util.Scanner;
 
-class PreLogin{
-    int pilihan = 0;
-    boolean adminStatus = false;
-
-    void inputPilihan(){
-        boolean isValid = false;
-        while(!isValid){
-            System.out.printf("" +
-                    "Pilih login:\n" +
-                    "1. Admin\n" +
-                    "2. Mahasiswa\n" +
-                    "Masukkan pilihan: ");
-            try{
-                Scanner input = new Scanner(System.in);
-                pilihan = input.nextInt();
-                switch (pilihan){
-                    case 1: adminStatus = true; isValid = true; break;
-                    case 2: isValid = true; break;
-                    default: System.out.println("Pilihan tidak valid");
-                }
-            }catch(java.util.InputMismatchException e){
-                System.out.println("Input tidak valid");
-            }
-        }
-    }
-
-    boolean isAdmin(){
-        return adminStatus;
+public class Main {
+    public static void main(String[] args) {
+        LoginSystem loginSystem = new LoginSystem();
+        loginSystem.tampilkanMenu();
     }
 }
 
-class Login{
-    String adminName = "Admin256",
-            adminPass = "Password256";
-    String studentName = "Muhammad Budi Kusuma",
-            studentPass = "202410370110256";
-    String nameFormat, passFormat, statusFormat;
-    boolean status, isValid = false;
+class LoginSystem{
+    Admin admin = new Admin();
+    Mahasiswa mahasiswa = new Mahasiswa();
 
-    void format (String username, String password, String adminOrstudent, Boolean isAdmin){
-        nameFormat = username;
-        passFormat = password;
-        statusFormat = adminOrstudent;
-        status = isAdmin;
-    }
+   void tampilkanMenu(){
+       Scanner input = new Scanner(System.in);
+       int pilihan;
+       System.out.printf("" +
+               "Pilih login:\n" +
+               "1. Admin\n" +
+               "2. Mahasiswa\n" +
+               "Masukkan pilihan: ");
+       pilihan = input.nextInt();
+       input.nextLine();
 
-    void input(Scanner input){
-        System.out.printf("Masukkan %s: ", nameFormat); String inUsername = input.nextLine();
-        System.out.printf("Masukkan %s: ", passFormat); String inPassword = input.nextLine();
+       switch (pilihan){
+           case 1:
+               String username, password;
+               System.out.printf("Masukkan username: "); username = input.nextLine();
+               System.out.printf("Masukkan password: "); password = input.nextLine();
+               admin.login(username, password);
+               if(admin.getAdminValid() == true){
+                   System.out.printf("Login Admin berhasil!");
+               }else{
+                   System.out.printf("Login gagal! Username atau Password salah.");
+               } break;
 
-        if(status){
-            if(inUsername.equals(adminName) && inPassword.equals(adminPass)){
-                isValid = true;
-            }
-        }else{
-            if(inUsername.equals(studentName) && inPassword.equals(studentPass)){
-                isValid = true;
-            }
-        }
-    }
-
-    void status(){
-        if(isValid){
-            System.out.printf("Login %s berhasil!", statusFormat);
-        }else{
-            System.out.printf("Login gagal! %s atau %s salah.", nameFormat, passFormat);
-        }
-    }
+           case 2:
+               String nama, nim;
+               System.out.printf("Masukkan nama: "); nama = input.nextLine();
+               System.out.printf("Masukkan NIM: "); nim = input.nextLine();
+               mahasiswa.login(nama, nim);
+               if(mahasiswa.getAdminValid() == true){
+                   System.out.printf("Login Mahasiswa berhasil!\n");
+                   mahasiswa.displayInfo();
+               }else{
+                   System.out.printf("Login gagal! Nama atau NIM salah.");
+               } break;
+           default:
+               System.out.printf("Pilihan tidak valid.");
+       }
+   }
 }
 
-class LoginAdmin{
+class Admin{
     String username = "Admin256";
     String password = "Password256";
     boolean isValid = false;
 
-    void input(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Masukkan username: "); String inUsername = input.nextLine();
-        System.out.printf("Masukkan password: "); String inPassword = input.nextLine();
-
-        if(inUsername.equals(username) && inPassword.equals(password)){
+    void login(String nameuser, String wordpass){
+        if(nameuser.equals(username) && wordpass.equals(password)){
             isValid = true;
         }
     }
 
-    void status(){
-        if(isValid){
-            System.out.printf("Login Admin berhasil!");
-        }else{
-            System.out.printf("Login gagal! Username atau Passowrd salah.");
-        }
-    }
+    public boolean getAdminValid(){ return isValid; }
 }
 
-class LoginMahasiswa{
+class Mahasiswa{
     String nama = "Muhammad Budi Kusuma";
     String nim = "202410370110256";
     boolean isValid = false;
 
-    void input(){
-        Scanner input = new Scanner(System.in);
-        System.out.printf("Masukkan Nama: "); String inNama = input.nextLine();
-        System.out.printf("Masukkan NIM: "); String inNIM = input.nextLine();
-
-        if(inNama.equals(nama) && inNIM.equals(nim)){
+    void login(String mana, String min){
+        if(mana.equals(nama) && min.equals(nim)){
             isValid = true;
         }
-
     }
 
-    void status(){
-        if(isValid){
-            System.out.printf("Login Mahasiswa berhasil!");
-        }else{
-            System.out.printf("Login gagal! Nama atau NIM salah.");
-        }
+    public boolean getAdminValid(){ return isValid; }
+
+    void displayInfo(){
+        System.out.printf("Nama: %s\nNIM: %s\n", nama, nim);
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
-        PreLogin prelogin = new PreLogin();
-        prelogin.inputPilihan();
-
-        if(prelogin.isAdmin() == true){
-//            LoginAdmin loginAdmin = new LoginAdmin();
-//            loginAdmin.input(); loginAdmin.status();
-
-            Login login = new Login();
-            login.format("username", "password", "Admin", prelogin.isAdmin());
-            login.input(new Scanner(System.in));
-            login.status();
-        }else{
-//            LoginMahasiswa loginMahasiswa = new LoginMahasiswa();
-//            loginMahasiswa.input(); loginMahasiswa.status();
-
-            Login login = new Login();
-            login.format("Nama", "NIM","Mahasiswa", prelogin.isAdmin());
-            login.input(new Scanner(System.in));
-            login.status();
-        }
-    }
-}
-
-//Update!
