4bf107b66305c0096d0c5608485c874fcb6be825
MBOEDIK65 <boediksm65@gmail.com>
2025-03-18 01:14:37 +0700

    ngerjain codelab 2

--- Codelab/Modul_2/Codelab 2/src/Main.java
+++ Codelab/Modul_2/Codelab 2/src/Main.java
@@ -1,5 +1,52 @@
+class RekeningBank{
+    String nomorRekening, namaPemilik;
+    double saldo;
+
+    void tampilkanInfo(){
+        System.out.printf("Nomor Rekening: %s\n", nomorRekening);
+        System.out.printf("Nama Pemilik: %s\n", namaPemilik);
+        System.out.printf("Saldo: Rp%.1f\n\n", saldo);
+    }
+
+    void setorUang(double jumlah){
+        saldo += jumlah;
+        System.out.printf("%s menyetor Rp%.1f. Saldo sekarang: Rp%.1f\n", namaPemilik, jumlah, saldo);
+    }
+
+    void tarikUang(double jumlah){
+        if(saldo < jumlah){
+            System.out.printf("%s menarik %.1f. (Gagal, saldo tidak mencukupi) Saldo saat ini: %.1f\n", namaPemilik, jumlah, saldo);
+        }else{
+            saldo -= jumlah;
+            System.out.printf("%s menarik %.1f. (Berhasil) Saldo saat ini: %.1f\n", namaPemilik, jumlah, saldo);
+        }
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        RekeningBank rekening1 = new RekeningBank();
+        RekeningBank rekening2 = new RekeningBank();
+
+        rekening1.nomorRekening = "202410370110256";
+        rekening1.namaPemilik = "Muhammad Budi Kusuma";
+        rekening1.saldo = 30000;
+        rekening1.tampilkanInfo();
+
+        rekening2.nomorRekening = "202410370110258";
+        rekening2.namaPemilik = "M Juan Yafi Zain Wibisono";
+        rekening2.saldo = 75000;
+        rekening2.tampilkanInfo();
+
+        rekening1.setorUang(20000);
+        rekening2.setorUang(100000);
+        System.out.printf("\n");
+        rekening1.tarikUang(40000);
+        rekening2.tarikUang(200000);
+        System.out.printf("\n");
+        rekening1.tampilkanInfo();
+        rekening2.tampilkanInfo();
+
     }
 }
